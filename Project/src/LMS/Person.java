ced699ff6965de6d87381c22791f7977476a6e02
liyangxx <liyangchoong@gmail.com>
2024-01-10 00:34:51 +0800

    fixed Possible unsafe assignment to a non-final static field in a constructor issue in Person..java

--- Project/src/LMS/Person.java
+++ Project/src/LMS/Person.java
@@ -14,11 +14,10 @@ public abstract class Person
 
     public Person(int idNum, String name, String address, int phoneNum)   // para cons.
     {
-        currentIdNumber++;
         
         if(idNum==-1)
         {
-            id = currentIdNumber;
+            id = currentIdNumber + 1;
         }
         else
             id = idNum;
